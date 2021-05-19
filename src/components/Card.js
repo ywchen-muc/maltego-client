0763a4b917fc1572d17cd5c66ab1656dc3e617ef
Vivienne Chen <viviennechen@Viviennede-MacBook-Pro.fritz.box>
2021-05-19 23:46:21 +0200

    Adjust display of graphs

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,92 +1,24 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import Graphin from '@antv/graphin';
+import Graphin, { Behaviors } from '@antv/graphin';
 
 const Card = ({ id, name, data, onRemove }) => {
+    const { ZoomCanvas } = Behaviors;
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 bw2 shadow-5'>
+        <div className="bg-light-green w-30 dib br3 pa3 ma2 bw2 shadow-5">
             <div>
-              <Graphin data={data} />
-              <h2>{id}</h2>
-              <h2>{name}</h2>
-              <button 
+                <h2>{name}</h2>
+            </div>
+                <Graphin data={data} layout={{ type: 'concentric' }} width="300px" >
+                 <ZoomCanvas disabled />
+                 </Graphin>
+            <button 
                 className='f6 link dim br2 ba bw1 ph3 pv2 mb2 dib mid-gray' 
                 onClick={() => onRemove(id)}>
                 Delete
-              </button>
-            </div>
+            </button>
         </div>
     );
 }
 
 export default Card;
-
-// export default function () {
-//     const data = {
-//         nodes: [
-//           {
-//             id: 'node1',
-//             label: 'Circle1',
-//             x: 150,
-//             y: 150,
-//           },
-//           {
-//             id: 'node2',
-//             label: 'Circle2',
-//             x: 400,
-//             y: 150,
-//           },
-//         ],
-//         edges: [
-//           {
-//             source: 'node1',
-//             target: 'node2',
-//           },
-//         ],
-//       };
-      
-//     const ref = React.useRef(null);
-//     let graph = null;
-  
-//     useEffect(() => {
-//       if (!graph) {
-//         // Instantiate the Graph
-//         graph = new G6.Graph({
-//           container: ReactDOM.findDOMNode(ref.current),
-//           width: 300,
-//           height: 500,
-//           modes: {
-//             default: ['drag-canvas'],
-//           },
-//           layout: {
-//             type: 'dagre',
-//             direction: 'LR',
-//           },
-//           defaultNode: {
-//             type: 'node',
-//             labelCfg: {
-//               style: {
-//                 fill: '#000000A6',
-//                 fontSize: 10,
-//               },
-//             },
-//             style: {
-//               stroke: '#72CC4A',
-//               width: 150,
-//             },
-//           },
-//           defaultEdge: {
-//             type: 'polyline',
-//           },
-//         });
-//       }
-//       graph.data(data);
-//       graph.render();
-//     }, []);
-  
-//     return (
-//         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' ref={ref}>
-//             <h2>Graph</h2>
-//         </div>
-//         );
-//   }
